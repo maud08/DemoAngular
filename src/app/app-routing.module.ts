b17a198307d6ca13c781d07381c915170a42e058
maud08 <maudinette08@outlook.com>
2021-11-02 12:20:55 +0100

    exercice navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { EnfantComponent } from './home/enfant/enfant.component';
 import { HomeComponent } from './home/home.component';
+import { NouveauModule } from './nouveau/nouveau.module';
 
 const routes: Routes = [
   {path : 'home', component : HomeComponent, children: [
     {path: 'enfant', component: EnfantComponent}
   ]},
-  {path : 'about', component : AboutComponent}
+  {path : 'about', component : AboutComponent},
+  {path : 'nouveau', loadChildren: () => import('./nouveau/nouveau.module').then(m => m.NouveauModule)},
+  {path : 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)},
+  {path: 'exercice', loadChildren: () => import('./exercice/exercice.module').then(m => m.ExerciceModule)}
 ];
 
 @NgModule({
